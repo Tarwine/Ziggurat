acbafcac47b76283a9c3f1ac7419fa4abe16a29e
Tarwine <tarwine0406@gmail.com>
2014-01-26 16:40:11 -0500

    Sparkles!

--- Assets/Scripts/FadeScripts/CameraFade.cs
+++ Assets/Scripts/FadeScripts/CameraFade.cs
@@ -36,7 +36,7 @@ public class CameraFade : MonoBehaviour
 			times += Time.deltaTime;
 		}
 		//Debug.Log("duh");
-		if(times > 3.0f){
+		if(times > fadeTime - 5.0f){
 			Debug.Log ("WE HAVE ARRIVED");
 			Application.LoadLevel(Application.loadedLevel + 1);
 		}
---
+++ Assets/Scripts/SparkleShot.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SparkleShot : MonoBehaviour {
+	public GameObject explosion;
+	public GameObject platformArea;
+	public GameObject explosionSpawn;
+	public GameObject areaSpawn;
+	private bool hasFired = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other){
+		if(!hasFired){
+			Instantiate(explosion, explosionSpawn.transform.position, explosionSpawn.transform.rotation);
+			Instantiate(platformArea, areaSpawn.transform.position, areaSpawn.transform.rotation);
+			Destroy(GameObject.Find("gemOBJ"));
+			Debug.Log("Took the Cat");
+			hasFired = true;
+		}
+	}
+}
